b862ec2e7283a58a2f0958cbce2de8a58c4a93d8
gabrielVolponi <0gabrielvolponi0@gmail.com>
2025-08-19 19:24:36 -0300

    ajuste imagem quarto 3 camas

--- yoga/src/conteudo/cardsPreco.js
+++ yoga/src/conteudo/cardsPreco.js
@@ -4,7 +4,7 @@ export const cards = [
       subtitle: "p/ ATÉ 3 PESSOAS COM TODAS AS REFEIÇÕES",
       priceLine: "R$ 2.880 por pessoa em até 3x sem juros no Pix",
       installmentLine: "Ou no cartão em até 12x com taxas.",
-      imgSrc: "/assets/local/quarto/quartoAzul.jpg",
+      imgSrc: "/assets/local/quarto/quarto3.jpg",
       imgAlt: "Foto do quarto azul compartilhado para até 3 pessoas"
     },
     {
