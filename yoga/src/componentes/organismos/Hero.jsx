91c40c0c4756e7068d3d807a6e30cc16e4a605bd
gabrielVolponi <0gabrielvolponi0@gmail.com>
2025-08-19 19:39:30 -0300

    ajustes para xl hero

--- yoga/src/componentes/organismos/Hero.jsx
+++ yoga/src/componentes/organismos/Hero.jsx
@@ -18,33 +18,33 @@ const facilitadores = [
 export default function Hero() {
   return (
     <div className="h-dvh px-4 lg:px-20" id="hero">
-      <div className="w-full flex flex-col items-center justify-center mt-10 md:mt-8 xl:mt-10">
-        <div className="mt-30 my-4 xl:mt-35">
+      <div className="w-full flex flex-col items-center justify-center mt-10 md:mt-8 2xl:mt-10">
+        <div className="mt-30 my-4 2xl:mt-35">
           <div className="bg-white w-full mx-auto h-0.5"></div>
-          <p className="text-center text-sm text-white sm:text-base lg:text-lg xl:text-2xl">
+          <p className="text-center text-sm text-white sm:text-base lg:text-lg 2xl:text-2xl">
             7, 8 E 9 DE NOVEMBRO
           </p>
           <div className="bg-white w-full mx-auto h-0.5"></div>
         </div>
-        <h1 className="text-center text-4xl w-full mt-8 mb-2 font-[tan] text-white sm:w-1/2 lg:text-5xl xl:text-[60px]">
+        <h1 className="text-center text-4xl w-full mt-8 mb-2 font-[tan] text-white sm:w-1/2 lg:text-5xl 2xl:text-[60px]">
           Retiro <br /> Yoga & <br /> Magia <br /> Natural
         </h1>
-        <h4 className="text-base text-white sm:text-lg lg:text-xl xl:text-2xl">Versão 3.0</h4>
-        <p className="text-lg font-semibold w-full text-center my-4 font-[tan] text-white sm:text-2xl sm:w-2/3 sm:my-6 lg:text-2xl xl:text-3xl ">
+        <h4 className="text-base text-white sm:text-lg lg:text-xl 2xl:text-2xl">Versão 3.0</h4>
+        <p className="text-lg font-semibold w-full text-center my-4 font-[tan] text-white sm:text-2xl sm:w-2/3 sm:my-6 lg:text-2xl 2xl:text-3xl ">
           A reconexão com <br /> o amor verdadeiro
         </p>
         <div className="w-full sm:w-9/10 lg:w-3/4 flex justify-center">
           {facilitadores.map((facilitador) => (
             <div className="flex flex-col items-center justify-center mb-4 sm:mb-0 lg:mx-4">
               <img
-                className="w-25 h-25 sm:w-25 sm:h-25 lg:w-32 lg:h-32 object-cover rounded-full xl:w-50 xl:h-50"
+                className="w-25 h-25 sm:w-25 sm:h-25 lg:w-32 lg:h-32 object-cover rounded-full 2xl:w-50 2xl:h-50"
                 src={facilitador.foto}
                 alt={facilitador.nome}
               />
               <h3 className="text-base sm:text-lg text-white font-semibold lg:text-xl ">
                 {facilitador.nome}
               </h3>
-              <p className="text-xs text-white sm:text-sm lg:text-base w-4/5 sm:w-2/3 md:w-full text-center">
+              <p className="text-xs text-white text-center sm:text-sm lg:text-base w-4/5 sm:w-2/3 md:w-full ">
                 {facilitador.cargo}
               </p>
             </div>
