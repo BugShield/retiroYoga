5c38de4a5baf5956b59c16383014c7665f57389e
gabrielVolponi <0gabrielvolponi0@gmail.com>
2025-08-19 18:45:03 -0300

    ajuste facilitadores

--- yoga/src/componentes/organismos/Facilitadores.jsx
+++ yoga/src/componentes/organismos/Facilitadores.jsx
@@ -5,29 +5,31 @@ export default function Facilitadores() {
   return (
     <div id="facilitadores">
       <div
-        className="relative bg-no-repeat bg-center bg-cover pt-4 xl:h-dvh xl:flex xl:items-center xl:justify-center 2xl:pt-8"
+        className="relative bg-no-repeat bg-center bg-cover pt-4 xl:h-dvh "
         style={{ backgroundImage: `url(${bgHeart})` }}
       >
-        <h2 className="font-bold text-[22px] my-8 font-[tan] text-center xl:text-[26px]">
-          Facilitador
+        <h2 className="font-bold text-[22px] my-8 font-[tan] text-center xl:mt-20 xl:text-[26px]">
+          Facilitadores
         </h2>
-        {facilitadores.map((facilitador) => (
-          <div className="h-dvh mt-4 flex flex-col items-center xl:h-auto xl:max-w-4xl 2xl:max-w-6xl xl:mx-auto">
-            <img
-              className="w-[150px] h-[150px] rounded-full object-cover xl:w-[250px] xl:h-[250px]"
-              src={facilitador.foto}
-              alt={facilitador.nome}
-            />
-            <h4 className="my-4 font-bold text-lg text-white">{facilitador.nome}</h4>
-            <div className="mt-4 flex flex-col gap-4 text-white">
-              {facilitador.paragrafos.map((paragrafo) => (
-                <p className="flex flex-col gap-4 text-[16px] w-8/10 mx-auto xl:w-1/2">
-                  {paragrafo}
-                </p>
-              ))}
+        <div className="xl:flex xl:items-center 2xl:pt-8">
+          {facilitadores.map((facilitador) => (
+            <div className="h-dvh mt-4 flex flex-col items-center xl:h-auto 2xl:max-w-6xl xl:mx-auto">
+              <img
+                className="w-[150px] h-[150px] rounded-full object-cover xl:w-[250px] xl:h-[250px]"
+                src={facilitador.foto}
+                alt={facilitador.nome}
+              />
+              <h4 className="my-4 font-bold text-lg text-white">{facilitador.nome}</h4>
+              <div className="mt-4 flex flex-col gap-4 text-white">
+                {facilitador.paragrafos.map((paragrafo) => (
+                  <p className="flex flex-col gap-4 text-[16px] w-8/10 mx-auto xl:w-1/2">
+                    {paragrafo}
+                  </p>
+                ))}
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
