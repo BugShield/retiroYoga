9853ff71155c0f3a1d4985120b4bf275ea8959fa
gabrielVolponi <0gabrielvolponi0@gmail.com>
2025-08-20 16:45:05 -0300

    ajustes gerais de scroll e facilitadores

--- yoga/src/componentes/organismos/Facilitadores.jsx
+++ yoga/src/componentes/organismos/Facilitadores.jsx
@@ -3,12 +3,12 @@ import bgHeart from "/assets/bgHearts.png";
 
 export default function Facilitadores() {
   return (
-    <div id="facilitadores">
+    <div id="facilitadores" className="scroll-mt-24">
       <div
         className="relative bg-no-repeat bg-center bg-cover pt-4 xl:h-dvh "
         style={{ backgroundImage: `url(${bgHeart})` }}
       >
-        <h2 className="font-bold text-[22px] mt-8 font-[tan] text-center xl:mt-20 xl:text-[26px]">
+        <h2 className="font-bold text-[22px] mt-4 font-[tan] text-center xl:mt-20 xl:text-[26px]">
           Facilitadores
         </h2>
         <div className="xl:h-dvh xl:flex xl:justify-center xl:gap-x-12 2xl:pt-8 xl:max-w-5xl xl:mx-auto">
@@ -22,7 +22,7 @@ export default function Facilitadores() {
               <h4 className="my-4 font-bold text-lg text-white">{facilitador.nome}</h4>
               <div className="mt-4 flex flex-col gap-4 text-white">
                 {facilitador.paragrafos.map((paragrafo) => (
-                  <p className="flex flex-col gap-4 text-[16px] w-8/10 mx-auto xl:w-full">
+                  <p className="flex flex-col gap-4 text-[16px] w-9/10 mx-auto xl:w-full">
                     {paragrafo}
                   </p>
                 ))}
--- yoga/src/componentes/organismos/Imersão.jsx
+++ yoga/src/componentes/organismos/Imersão.jsx
@@ -3,10 +3,10 @@ import CarroselFotos from "../moleculas/CarroselFotos";
 
 export default function Imersao() {
   return (
-    <div id="imersao" className="pb-10">
+    <div id="imersao" className="scroll-mt-24">
       <div className="">
         <div className="flex flex-col gap-4 text-[16px] w-9/10 mx-auto text-white xl:mx-auto  xl:text-center">
-          <h2 className="font-bold text-[22px] ml-1 mb-0 mt-20 font-[tan]  xl:text-2xl">
+          <h2 className="font-bold text-[22px] ml-1 mb-0 mt-4 font-[tan]  xl:text-2xl">
             {imersao.titulo}
           </h2>
           <div className="flex flex-col gap-4 xl:text-[20px] ">
@@ -21,7 +21,7 @@ export default function Imersao() {
         <h3 className="text-[19px] font-bold mb-8 underline font-[tan] xl:text-2xl">
           {imersao.cta}
         </h3>
-        <ul className="mx-auto font-semibold text-start">
+        <ul className="mx-auto font-semibold text-start pb-10 ">
           {imersao.atividades.map((atividade) => (
             <li className="list-disc xl:text-[19px]">{atividade}</li>
           ))}
