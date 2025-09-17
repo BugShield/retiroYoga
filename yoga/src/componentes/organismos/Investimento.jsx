096152eca00952cc2c02ab392f289ce9daa0d743
gabrielVolponi <0gabrielvolponi0@gmail.com>
2025-09-17 09:53:44 -0300

    removendo modal de texto legal + removendo option de pix parcelado

--- yoga/src/componentes/organismos/Investimento.jsx
+++ yoga/src/componentes/organismos/Investimento.jsx
@@ -2,25 +2,36 @@ import CardsPreco from "../moleculas/CardsPreco";
 import { cards } from "../../conteudo/cardsPreco";
 
 const textosInvestimento = {
-  titulo: 'Opções de Hospedagem',
-  paragrafo: 'Escolha a melhor opção para sua estadia. Valores por pessoa.',
-}
+  titulo: "Opções de Hospedagem",
+  paragrafo: "Escolha a melhor opção para sua estadia. Valores por pessoa.",
+};
 
 export default function Investimento() {
   return (
     <div id="investimento" className="h-auto scroll-mt-24">
       <div className=" max-w-6xl mx-auto px-4 md:text-center lg:h-auto">
         <header className="mt-4 mb-8">
-          <h1 className="text-2xl font-bold text-white ml-2 xl:text-2xl ">{textosInvestimento.titulo}</h1>
-          <p className="mt-1 ml-2 text-white">
-            {textosInvestimento.paragrafo}
-          </p>
+          <h1 className="text-2xl font-bold text-white ml-2 xl:text-2xl ">
+            {textosInvestimento.titulo}
+          </h1>
+          <p className="mt-1 ml-2 text-white">{textosInvestimento.paragrafo}</p>
         </header>
 
-        <div className="grid grid-cols-1 gap-4 pb-10 md:grid-cols-2 xl:pb-20">
+        <div className="grid grid-cols-1 gap-4 pb-6 md:grid-cols-2">
           {cards.map((c, i) => (
             <CardsPreco key={i} {...c} />
           ))}
+        </div>
+
+        <div className="flex flex-col items-center gap-2 pb-8">
+          <h2 className="font-bold text-lg underline">Aviso Legal</h2>
+          <p className="w-10/11 text-sm">
+            O participante tem o direito de cancelar a compra do ingresso até 7
+            dias após a data da compra, desde que o cancelamento ocorra com pelo
+            menos 30 dias de antecedência da data do evento. Após esse período,
+            não serão aceitos pedidos de reembolso. Em caso de não
+            comparecimento ou desistência, não haverá reembolso do valor pago.
+          </p>
         </div>
       </div>
     </div>
